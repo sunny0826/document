91e79aab9cb0156df8605a0b108184774377758c
zhuangbiaowei <zhuangbiaowei@gmail.com>
2021-05-13 14:34:01 +0800

    添加PRD文档

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,7 @@
 module.exports = {
   docs: {
-    Introduction: ['introduction/getting-started'],
+    "介绍": ['introduction/getting-started'],
+    "产品文档":['ProductDoc/index'],
     // 'API Reference': ['api', 'api-queries'],
     // Guides: ['migration-v7', 'migration-v2', 'how-should-i-query'],
     // Examples: ['react-navigation', 'redux-integration'],
